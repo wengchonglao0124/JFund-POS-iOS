fcd0723f1b76c6d6a8b561602f649459f6013bf4
Weng Chong <wengchonglao0124@gmail.com>
2022-11-24 02:05:37 +1000

    - Add Promotion View

--- Jacaranda Card POS/Views/JacarandaPromotionView.swift
+++ Jacaranda Card POS/Views/JacarandaPromotionView.swift
@@ -8,13 +8,120 @@
 import SwiftUI
 
 struct JacarandaPromotionView: View {
+    
+    @State var selectedTab = "all"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center, spacing: 0) {
+            // MARK: Title Section
+            HStack(spacing: 0) {
+                Text("Promotion")
+                    .font(Font.custom("DMSans-Bold", size: 18))
+                    .padding(.leading, 22)
+                    .padding(.bottom, 33)
+                Spacer()
+            }
+            
+            // MARK: Request Promotion Section
+            Button {
+                print("Request Promotion")
+            } label: {
+                Image("requestPromotionButton")
+            }
+
+            // MARK: Picker Section
+            HStack(alignment: .center, spacing: 25) {
+                // MARK: All Tab
+                Text("All")
+                    .font(Font.custom("DMSans-Bold", size: 12))
+                    .foregroundColor({
+                        if selectedTab == "all" {
+                            return Color("promotionPickerSelectedColor")
+                        }
+                        else {
+                            return Color("promotionPickerColor")
+                        }
+                    }())
+                    .underline(selectedTab == "all")
+                    .onTapGesture {
+                        withAnimation(.interactiveSpring(
+                            response: 0.5, dampingFraction: 0.6, blendDuration: 0.6)){
+                                selectedTab = "all"
+                            }
+                    }
+                
+                // MARK: Active Tab
+                Text("Active")
+                    .font(Font.custom("DMSans-Bold", size: 12))
+                    .foregroundColor({
+                        if selectedTab == "active" {
+                            return Color("promotionPickerSelectedColor")
+                        }
+                        else {
+                            return Color("promotionPickerColor")
+                        }
+                    }())
+                    .underline(selectedTab == "active")
+                    .onTapGesture {
+                        withAnimation(.interactiveSpring(
+                            response: 0.5, dampingFraction: 0.6, blendDuration: 0.6)){
+                                selectedTab = "active"
+                            }
+                    }
+                
+                // MARK: Pending Tab
+                Text("Pending")
+                    .font(Font.custom("DMSans-Bold", size: 12))
+                    .foregroundColor({
+                        if selectedTab == "pending" {
+                            return Color("promotionPickerSelectedColor")
+                        }
+                        else {
+                            return Color("promotionPickerColor")
+                        }
+                    }())
+                    .underline(selectedTab == "pending")
+                    .onTapGesture {
+                        withAnimation(.interactiveSpring(
+                            response: 0.5, dampingFraction: 0.6, blendDuration: 0.6)){
+                                selectedTab = "pending"
+                            }
+                    }
+                
+                // MARK: Expired Tab
+                Text("Expired")
+                    .font(Font.custom("DMSans-Bold", size: 12))
+                    .foregroundColor({
+                        if selectedTab == "expired" {
+                            return Color("promotionPickerSelectedColor")
+                        }
+                        else {
+                            return Color("promotionPickerColor")
+                        }
+                    }())
+                    .underline(selectedTab == "expired")
+                    .onTapGesture {
+                        withAnimation(.interactiveSpring(
+                            response: 0.5, dampingFraction: 0.6, blendDuration: 0.6)){
+                                selectedTab = "expired"
+                            }
+                    }
+                
+                Spacer()
+            }
+            .padding(.leading, 33)
+            .padding(.vertical, 28)
+        }
     }
 }
 
 struct JacarandaPromotionView_Previews: PreviewProvider {
     static var previews: some View {
-        JacarandaPromotionView()
+        Group {
+            JacarandaPromotionView()
+            
+            JacarandaPromotionView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone SE (3rd generation)"))
+        }
     }
 }
